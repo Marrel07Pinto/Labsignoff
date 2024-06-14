a98e0af184d8b8e1ad8df5e1b9068eda60a61c59
marrel <marrel7pinto@gmail.com>
2024-06-14 19:59:01 +0100

    added namespace in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SeatController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/seat',function(){
+    return view('seat');
+});
+
+Route::post('/seat_value', [SeatController::class, 'store'])->name('seat_value');
+
 
 require __DIR__.'/auth.php';

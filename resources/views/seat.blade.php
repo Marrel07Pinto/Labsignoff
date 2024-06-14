fe41cdbd1080eb89f4875ec211416980562c961e
marrel <marrel7pinto@gmail.com>
2024-06-14 17:47:13 +0100

    Frontend seat interface was created

---
+++ resources/views/seat.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Boxes</title>
+</head>
+<body>
+
+<button style="background-color:red; color:white;">S1</button>
+<button>S2</button>
+<button>S3</button>
+<button>S4</button>
+<button>S5</button>
+<br>
+<button>S10</button>
+<button>S9</button>
+<button>S8</button>
+<button style="background-color:red; color:white;">S7</button>
+<button>S6</button>
+
+</body>
+</html>

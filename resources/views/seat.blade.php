1db916a27ef412f87c5e0e8cdebc9e350dcd61ae
marrel <marrel7pinto@gmail.com>
2024-06-19 17:55:20 +0100

    Added update button for the seat change

--- resources/views/seat.blade.php
+++ resources/views/seat.blade.php
@@ -6,33 +6,51 @@
         <main id="main" class="main">
 
             <div class="pagetitle">
-            <h1>Please select your seat</h1>
-            <nav>
-                <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                <li class="breadcrumb-item">Icons</li>
-                <li class="breadcrumb-item active">Bootstrap</li>
-                </ol>
-            </nav>
+                <h1>Please select your seat</h1>
+                <nav>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item">Icons</li>
+                        <li class="breadcrumb-item active">Bootstrap</li>
+                    </ol>
+                </nav>
             </div><!-- End Page Title -->
 
             <section class="section">
+                @if(session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+                @if(session('info'))
+                    <div class="alert alert-info">
+                        {{ session('info') }}
+                            @php
+                                $user_id = auth()->user()->id;
+                             @endphp
+                         <form id="seatForm" action="{{ url('/seat_vupdate/' . $user_id) }}" method="POST">
+                            @csrf
+                                <button type="submit" name="seat_number" value="{{ session('seat_number') }}">Yes</button>
+                        </form>
+                    </div>
+                @endif
+
                 <form id="seatForm" action="{{ route('seat_value') }}" method="POST">
                     @csrf
-                        <button type="submit" name="seat_number" value="S1" onclick="confirmSeatSelection(event)">S1</button>
-                        <button type="submit" name="seat_number" value="S2" onclick="confirmSeatSelection(event)">S2</button>
-                        <button type="submit" name="seat_number" value="S3" onclick="confirmSeatSelection(event)">S3</button>
-                        <button type="submit" name="seat_number" value="S4" onclick="confirmSeatSelection(event)">S4</button>
-                        <button type="submit" name="seat_number" value="S5" onclick="confirmSeatSelection(event)">S5</button>
+                    <button type="submit" name="seat_number" value="S1">S1</button>
+                    <button type="submit" name="seat_number" value="S2">S2</button>
+                    <button type="submit" name="seat_number" value="S3">S3</button>
+                    <button type="submit" name="seat_number" value="S4">S4</button>
+                    <button type="submit" name="seat_number" value="S5">S5</button>
                 </form>
-            <br>
+                    <br>
                 <form id="seatForm2" action="{{ route('seat_value') }}" method="POST">
                     @csrf
-                        <button type="submit" name="seat_number" value="S10" onclick="confirmSeatSelection(event)">S10</button>
-                        <button type="submit" name="seat_number" value="S9" onclick="confirmSeatSelection(event)">S9</button>
-                        <button type="submit" name="seat_number" value="S8" onclick="confirmSeatSelection(event)">S8</button>
-                        <button type="submit" name="seat_number" value="S7" onclick="confirmSeatSelection(event)">S7</button>
-                        <button type="submit" name="seat_number" value="S6" onclick="confirmSeatSelection(event)">S6</button>
+                    <button type="submit" name="seat_number" value="S10">S10</button>
+                    <button type="submit" name="seat_number" value="S9">S9</button>
+                    <button type="submit" name="seat_number" value="S8">S8</button>
+                    <button type="submit" name="seat_number" value="S7">S7</button>
+                    <button type="submit" name="seat_number" value="S6">S6</button>
                 </form>
             </section>
         </main>

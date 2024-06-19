9b95b14393b22aa1c05dee301540ed19603ebe0a
marrel <marrel7pinto@gmail.com>
2024-06-19 17:54:12 +0100

    Route that calls seat update function

--- routes/web.php
+++ routes/web.php
@@ -8,24 +8,21 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
-Route::get('/seat', function(){
-    return view('seat');
-})->name('seat');
-
-
+Route::get('/seat', function()
+                    {
+                        return view('seat');
+                    })->name('seat');
 Route::post('/seat_value', [SeatController::class, 'store'])->name('seat_value');
+Route::post('/seat_vupdate/{id}', [SeatController::class, 'update'])->name('seat_vupdate');
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
 require __DIR__.'/auth.php';

90e97c30e524e0d04fb669ea218681dd1aad1ef4
marrel <marrel7pinto@gmail.com>
2024-06-20 11:30:53 +0100

    defined one to many relationship

--- app/Models/Query.php
+++ app/Models/Query.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Query extends Model
 {
     use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class,'id');
+    }
 }

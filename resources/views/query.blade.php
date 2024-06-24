8506fba7ad9cbbf1c46859d42fe8a66dc99db8c8
marrel <marrel7pinto@gmail.com>
2024-06-24 12:02:15 +0100

    Query. blade to display form for query

---
+++ resources/views/query.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+@section('content')
+<!DOCTYPE html>
+<html>
+    <body>
+        <main id="main" class="main">
+
+            <div class="pagetitle">
+                <h1>Queries</h1>
+                <nav>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item">Icons</li>
+                        <li class="breadcrumb-item active">Bootstrap</li>
+                    </ol>
+                </nav>
+            </div><!-- End Page Title -->
+            <form id="queryform" action="{{ route('query_form') }}" method="POST" encrypt="multipart/form-data">
+                @csrf
+                <label for="q_img">Image:</label>
+                    <input type="file" id="q_img" name="q_img"><br><br>
+                <label for="q_description">Description:</label>
+                    <textarea id="q_description" name="q_description"></textarea><br><br>
+                <label for="q_clink">Codeshare Link:</label>
+                    <input type="text" id="q_clink" name="q_clink"><br><br>
+                <input type="submit" value="Submit">
+            </form>
+        </main>
+    </body>
+</html>
+@endsection
+
+

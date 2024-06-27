038f511385fb20bc59d407df69830aa185760b60
marrel <marrel7pinto@gmail.com>
2024-06-27 11:06:39 +0100

    created the sign table

---
+++ database/migrations/2024_06_27_091052_create_signs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('signs', function (Blueprint $table) {
+            $table->id();
+            $table->biginteger('users_id')->unsigned();
+            $table->string('s_seat');
+            $table->string('s_clink')->nullable();
+            $table->string('s_img')->nullable();
+            $table->string('s_description');
+            $table->timestamps();
+
+            $table->foreign('users_id')->references('id')->on('users')
+            ->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('s_seat')->references('seat_num')->on('seats')
+            ->onDelete('cascade')->onUpdate('cascade');
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('signoffs');
+    }
+};

815fb59915d19558ecb7d7f89a42100b2d15fecb
marrel <marrel7pinto@gmail.com>
2024-06-27 11:07:18 +0100

    Gave the routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SeatController;
-use App\Http\Controllers\QueryController;
+use App\Http\Controllers\SignController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::get('/', function () {
@@ -12,8 +12,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/query', function () {
-    return view('query');});
+Route::get('/sign', function () {
+    return view('sign');});
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -29,6 +29,6 @@ Route::post('/seat_vupdate/{id}', [SeatController::class, 'update'])->name('seat
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
 
-Route::post('/query',[QueryController::class,'store'])->name('query_form');
+Route::post('/sign_form',[SignController::class,'store'])->name('sign_form');
 
 require __DIR__.'/auth.php';

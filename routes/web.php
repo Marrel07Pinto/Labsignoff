32df87ca00748ef6cc4112d343d831d4689fd35a
marrel <marrel7pinto@gmail.com>
2024-06-27 11:47:09 +0100

    added sign_off and query route in web

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SeatController;
 use App\Http\Controllers\SignController;
+use App\Http\Controllers\QueryController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::get('/', function () {
@@ -12,8 +13,6 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/sign', function () {
-    return view('sign');});
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -24,11 +23,19 @@ Route::get('/seat', function()
                     {
                         return view('seat');
                     })->name('seat');
+Route::get('/sign', function () 
+                    {
+                        return view('sign');
+                    });
+Route::get('/query', function () 
+                    {
+                        return view('query');
+                    });
 Route::post('/seat_value', [SeatController::class, 'store'])->name('seat_value');
 Route::post('/seat_vupdate/{id}', [SeatController::class, 'update'])->name('seat_vupdate');
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
-
+Route::post('/query_form',[QueryController::class,'store'])->name('query_form');
 Route::post('/sign_form',[SignController::class,'store'])->name('sign_form');
 
 require __DIR__.'/auth.php';

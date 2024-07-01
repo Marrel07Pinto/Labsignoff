544d696f969b8f5a0b1e8275fe77b6aa956c2826
marrel <marrel7pinto@gmail.com>
2024-07-01 11:30:01 +0100

    Created home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+<!DOCTYPE html>
+<html>
+    <body>
+        <main id="main" class="main">
+
+            <div class="pagetitle">
+                <h1>About lab</h1>
+            </div><!-- End Page Title -->
+
+            <section class="section">
+            
+  
+            </section>
+            </main>
+    </body>
+</html>

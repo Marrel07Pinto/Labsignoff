f5cefd6932964af04bda9f762b4d18969c4894db
marrel <marrel7pinto@gmail.com>
2024-07-08 18:29:03 +0100

    gave a link to seat.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -299,7 +299,7 @@
     </a>
   </li>
   <li class="nav-item">
-    <a class="nav-link collapsed" href="">
+    <a class="nav-link collapsed" href="{{ route('seat') }}">
     <i class="bi bi-person-square"></i>
       <span>Seat</span>
     </a>

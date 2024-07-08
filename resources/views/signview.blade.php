cf68695e52680e49776d80059989b45c6bd047d0
marrel <marrel7pinto@gmail.com>
2024-07-08 13:26:50 +0100

    changed the heading and added the attribute

--- resources/views/signview.blade.php
+++ resources/views/signview.blade.php
@@ -18,14 +18,15 @@
 </head>
 <body>
     <div class="container">
-        <h2>Sign View</h2>
-        
+        <h2>Sign-off request view</h2>
+        <p><Strong>Seat Number : </strong> {{ $signview->s_seat }} </p>
         <p><Strong>ScreenShots : </strong>
         <div class = "image-container">
             <img src="{{ asset('images/signoff_images/' . $signview->s_img) }}" alt="Screenshot"></p>
         </div>
-        <p><strong>Description :</strong> {{ $signview->s_description }}</p>
+        <p style="text-align: justify;"><strong>Description :</strong> {{ $signview->s_description }}</p>
         <p><strong>Code Link :</strong> {{ $signview->s_clink }}</p>
+        <p><strong>Sign-off given by :</strong> </p>
 
         <!-- Add more fields as necessary -->
     </div>

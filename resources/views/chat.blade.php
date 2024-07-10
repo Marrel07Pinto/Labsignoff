353e26ffd97dd5f5dffb3cc0b2e6c2c1b9bc4fa1
marrel <marrel7pinto@gmail.com>
2024-07-10 13:34:31 +0100

    dynamicalls displays messages from database

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -4,28 +4,35 @@
 <html>
     <body>
         <main id="main" class="main">
-
-            <div class="pagetitle">
-                <h1><center>Chat</center></h1>
-            </div><!-- End Page Title -->
-            <section class="section">
-                <div class="chat-container">
-                    <div class="chat-messages">
-                        <div class="message">
-                            <p>Hey, how are you?</p>
-                            <span>10:05 AM</span>
+                <div class="pagetitle">
+                    <h1><center>Chat</center></h1>
+                </div><!-- End Page Title -->
+                <section class="section">
+                    @foreach($chatmessages as $item) 
+                        <div class="chat-container">
+                            <div class="chat-messages">
+                                @if($item->users_id === Auth::id())
+                                    <div class="message right-message" style="text-align: right">
+                                        <span style="font-size: 60%">{{ $item->user->name }}</span>
+                                        <h4>{{ $item->c_messages }}<span style="font-size: 40%; margin-left: 10px;">{{ $item->created_at->toTimeString() }}</span></h4>                                    
+                                    </div>
+                                @else
+                                    <div class="message left-message" style="text-align: left">
+                                        <span style="font-size: 60%">{{ $item->user->name }}</span>
+                                        <h4>{{ $item->c_messages }}<span style="font-size: 40%; margin-left: 10px;">{{ $item->created_at->toTimeString() }}</span></h4>                                    
+                                    </div>
+                                @endif
+                            </div>
                         </div>
-                        <div class="message self">
-                            <p>I'm good, thanks!</p>
-                            <span>10:06 AM</span>
-                        </div>
-                    </div>
-                    <div class="chat-input">
-                        <input type="text" placeholder="Type a message...">
-                        <button>Send</button>
-                    </div>
-                </div>
-            </section>
+                    @endforeach
+                        <form id="chat_form" action="{{route('chat_form') }}" method="POST">
+                            @csrf
+                                <div class="chat-input">
+                                    <input type="text" placeholder="Type a message..." id="c_messages" name="c_messages" required>
+                                    <button>Send</button>
+                                </div>
+                        </form>
+                </section>           
         </main>
     </body>
 </html>

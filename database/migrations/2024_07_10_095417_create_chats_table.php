9d3c2171279775ff35ffd60c75cf58542d81e504
marrel <marrel7pinto@gmail.com>
2024-07-10 11:01:37 +0100

    created chat table and attributes

---
+++ database/migrations/2024_07_10_095417_create_chats_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('users_id')->unsigned();
+            $table->text('c_messages');
+            $table->timestamps();
+            $table->foreign('users_id')->references('id')->on('users')
+            ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};

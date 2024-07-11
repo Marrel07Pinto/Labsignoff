2f2f90c4d8d4df8c4f729d83b139cf3b73fad6a2
marrel <marrel7pinto@gmail.com>
2024-07-11 14:01:23 +0100

    code that stores information in database

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Feedback;
 
 class FeedbackController extends Controller
 {
@@ -27,7 +28,16 @@ class FeedbackController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $feedback = new Feedback();
+        $feedback->f_understanding = $request->input('understanding');
+        $feedback->f_confusing = $request->input('confusing');
+        $feedback->f_interesting =$request->input('interesting');
+        $feedback->f_engaging = $request->input('engaging');
+        $feedback->f_important = $request->input('important');
+        $feedback->f_overall = $request->input('overall');
+        $feedback->f_difficulty = $request->input('difficulty');
+        $feedback->save();
+        return back()->with('success', 'Feedback submitted successfully!');
     }
 
     /**

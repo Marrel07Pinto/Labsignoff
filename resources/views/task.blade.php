aae96723fe2b6efb164b3b9881540283902befaf
marrel <marrel7pinto@gmail.com>
2024-07-11 16:23:28 +0100

    created task.blade.php

---
+++ resources/views/task.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('content')
+<!DOCTYPE html>
+<html>
+    <body>
+        <main id="main" class="main">
+
+            <div class="pagetitle">
+                <h1>Task</h1>
+            </div><!-- End Page Title -->
+
+            <section class="section">
+            
+  
+            </section>
+            </main>
+    </body>
+</html>
+@endsection

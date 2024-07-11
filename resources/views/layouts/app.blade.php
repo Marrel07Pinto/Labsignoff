e8b2291682785cdafbad160d78692f2e660cb56a
marrel <marrel7pinto@gmail.com>
2024-07-11 19:26:30 +0100

    gave the route to profile.edit page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -238,7 +238,7 @@
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+              <a class="dropdown-item d-flex align-items-center" href="{{ route('profile.edit') }}">
                 <i class="bi bi-gear"></i>
                 <span>Account Settings</span>
               </a>

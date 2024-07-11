549d73e54b4f2b74d2596fab6822b0ddc61ff706
marrel <marrel7pinto@gmail.com>
2024-07-11 23:39:57 +0100

    deleted unwanted route

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@ use App\Http\Controllers\Auth\PasswordController;
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');

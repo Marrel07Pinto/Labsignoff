fc733302c827a7cc2a95860674efdd285afe842e
marrel <marrel7pinto@gmail.com>
2024-07-15 13:00:33 +0100

    displays multiple images id present

--- resources/views/signview.blade.php
+++ resources/views/signview.blade.php
@@ -21,9 +21,20 @@
         <h2>Sign-off request view</h2>
         <p><Strong>Seat Number : </strong> {{ $signview->s_seat }} </p>
         <p><Strong>ScreenShots : </strong>
-        <div class = "image-container">
-            <img src="{{ asset('images/signoff_images/' . $signview->s_img) }}" alt="Screenshot"></p>
-        </div>
+            <div class="image-container">
+        @php
+            $images = json_decode($signview->s_img);
+        @endphp
+
+        @if(!empty($images))
+            @foreach($images as $image)
+                <img src="{{ asset('images/signoff_images/' . $image) }}" alt="Screenshot">
+                <br>
+            @endforeach
+        @else
+            <p>No images uploaded</p>
+        @endif
+    </div>
         <p style="text-align: justify;"><strong>Description :</strong> {{ $signview->s_description }}</p>
         <p><strong>Code Link :</strong> {{ $signview->s_clink }}</p>
         <p><strong>Sign-off given by :</strong> </p>

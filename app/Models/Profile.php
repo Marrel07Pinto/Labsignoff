7858ef5bb1bf65c9e9f64a54a04b97f9cafe0823
marrel <marrel7pinto@gmail.com>
2024-07-23 12:05:48 +0100

    changed the relationshiop code that is used to match the id of users id

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -10,6 +10,6 @@ class Profile extends Model
     use HasFactory;
     public function user()
     {
-        return $this->belongsTo(User::class, 'id'); 
+        return $this->belongsTo(User::class, 'users_id', 'id'); 
     }
 }

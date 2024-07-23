b283d8e6186c88c1fac28c4d1f63a6d000a2a593
marrel <marrel7pinto@gmail.com>
2024-07-23 10:58:58 +0100

    added route to user profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -229,7 +229,7 @@
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="">
+              <a class="dropdown-item d-flex align-items-center" href="{{ route('usersprofile') }}">
                 <i class="bi bi-person"></i>
                 <span>My Profile</span>
               </a>

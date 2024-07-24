b4507e1a90cdba802841e48f15f8dec595da77ba
marrel <marrel7pinto@gmail.com>
2024-07-24 20:28:31 +0100

    admin task page that is used to upload task

---
+++ resources/views/admintask.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+@section('content')
+<body>
+    <main id="main" class="main">
+        <div class="pagetitle">
+            <h1>Please upload the task</h1>
+        </div>
+
+@endsection

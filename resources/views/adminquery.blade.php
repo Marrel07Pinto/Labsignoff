d3d20b25285f1956e8051f9f920488e4ba70bda6
marrel <marrel7pinto@gmail.com>
2024-07-30 11:42:39 +0100

    added the code that displays the queries

--- resources/views/adminquery.blade.php
+++ resources/views/adminquery.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 <style>
     .btn-size {
-    width: 25%; 
-    align-items: center;
-    justify-content: center;
+        width: 25%;
+        align-items: center;
+        justify-content: center;
     }
 </style>
 <main id="main" class="main">
@@ -12,30 +12,28 @@
         <h1>Raised Queries</h1>
     </div><!-- End Page Title -->
     </br>
-    @if($adminquerydata->isEmpty())
+    @if(empty($queriesGroupedByTA))
         <p>No Teaching Assistance</p>
     @else
         <section class="section">
             <div class="row flex-row flex-nowrap overflow-auto">
-                @foreach($adminquerydata as $index => $ta)
+                @foreach($queriesGroupedByTA as $group)
                     <div class="col-lg-6">
                         <a href="" class="text-decoration-none">
                             <div class="card">
                                 <div class="card-body">
-                                    <h5 class="card-title">{{ $ta->name }}</h5>
+                                    <h5 class="card-title">{{ $group['ta']->name }}</h5>
                                     <ol class="breadcrumb">
                                         <li class="breadcrumb-item">Teaching Assistant</li>
                                     </ol>
                                     <br>
-                                    @foreach($qdata as $key => $querydata)
-                                        @if($key % count($adminquerydata) == $index)
-                                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                                <button type="button" class="btn btn-dark btn-size">
-                                                    <i class="bi bi-person"></i>&nbsp;{{ $querydata->q_seat }}
-                                                </button> 
-                                                {{ Str::limit($querydata->q_query, 30, '...') }}
-                                            </div>
-                                        @endif
+                                    @foreach($group['queries'] as $querydata)
+                                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                            <button type="button" class="btn btn-dark btn-size">
+                                                <i class="bi bi-person"></i>&nbsp;{{ $querydata->q_seat }}
+                                            </button> 
+                                            {{ Str::limit($querydata->q_query, 30, '...') }}
+                                        </div>
                                     @endforeach
                                 </div>
                             </div>

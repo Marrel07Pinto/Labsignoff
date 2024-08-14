21f1d26c980fc81381c8f9d8496118cf40bc4cc3
marrel <marrel7pinto@gmail.com>
2024-08-14 14:54:47 +0100

    this blade is used when view option is clicked

--- resources/views/signview.blade.php
+++ resources/views/signview.blade.php
@@ -11,37 +11,41 @@
             padding: 20px;
         }
         .image-container img {
-        width: 400px; 
-        height: auto; 
-    }
+            width: 400px; 
+            height: auto; 
+        }
     </style>
 </head>
 <body>
     <div class="container">
-        <h2>Sign-off request view</h2>
-        <p><Strong>Seat Number : </strong> {{ $signview->s_seat }} </p>
-        <p><Strong>ScreenShots : </strong>
+        <h2>Sign-off Request View</h2>
+        <p><strong>Seat Number:</strong> {{ $signview->s_seat }}</p>
+        <p><strong>ScreenShots:</strong>
             <div class="image-container">
-        @php
-            $images = json_decode($signview->s_img);
-        @endphp
+                @php
+                    $images = json_decode($signview->s_img);
+                @endphp
 
-        @if(!empty($images))
-            @foreach($images as $image)
-                <img src="{{ asset('images/signoff_images/' . $image) }}" alt="Screenshot">
-                <br>
-            @endforeach
-        @else
-            <p>No images uploaded</p>
+                @if(!empty($images))
+                    @foreach($images as $image)
+                        <img src="{{ asset('images/signoff_images/' . $image) }}" alt="Screenshot">
+                        <br>
+                    @endforeach
+                @else
+                    <p>No images uploaded</p>
+                @endif
+            </div>
+        </p>
+        <p style="text-align: justify;"><strong>Description:</strong> {{ $signview->s_description }}</p>
+        <p><strong>Code Link:</strong> {{ $signview->s_clink }}</p>
+        <p><strong>Status:</strong> {{ $signview->s_result }}</p>
+        <p><strong>Teaching Assistance:</strong> {{ $signview->s_resolved_by }}</p>
+
+        @if($signview->s_result === 'unresolved')
+            <p><strong>Reason for not giving the lab sign-off:</strong> {{ $signview->s_reason }}</p>
         @endif
-    </div>
-        <p style="text-align: justify;"><strong>Description :</strong> {{ $signview->s_description }}</p>
-        <p><strong>Code Link :</strong> {{ $signview->s_clink }}</p>
-        <p><strong>Sign-off given by :</strong> </p>
 
         <!-- Add more fields as necessary -->
     </div>
-
 </body>
 </html>
-

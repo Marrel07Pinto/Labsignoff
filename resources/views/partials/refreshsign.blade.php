a4c054d91daca3267e3b93f8821125de49b05bb0
marrel <marrel7pinto@gmail.com>
2024-08-14 11:07:56 +0100

    this page is called that only refreshes the sign off requries not the entire page

---
+++ resources/views/partials/refreshsign.blade.php
@@ -0,0 +1,36 @@
+<div class="row flex-row flex-nowrap overflow-auto">
+    @foreach($signsGivenToTA as $group)
+        @if ($group['ta']->id == auth()->user()->id)
+        <div class="col-lg-6">
+            <a class="text-decoration-none">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $group['ta']->name }}</h5>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">Teaching Assistant</li>
+                        </ol>
+                        <br>
+                        @foreach($group['signs'] as $signdata)
+                            @if (empty($signdata->s_solution)) 
+                                <a href="#" class="sign-link" 
+                                   data-description="{{ $signdata->s_description }}" 
+                                   data-id="{{ $signdata->id }}" 
+                                   data-solution="{{ $signdata->s_solution }}" 
+                                   data-images="{{ json_encode($signdata->s_img ? json_decode($signdata->s_img) : []) }}">
+                                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                        <button type="button" class="btn btn-dark btn-size">
+                                            <i class="bi bi-person"></i>&nbsp;{{ $signdata->s_seat }}
+                                        </button> 
+                                        {{ Str::limit($signdata->s_description, 30, '...') }}
+                                        <!-- Container for images in the sign section -->
+                                    </div>
+                                </a>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            </a>
+        </div>
+        @endif
+    @endforeach
+</div>

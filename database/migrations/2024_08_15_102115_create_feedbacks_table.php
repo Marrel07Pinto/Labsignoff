4852e71b946872779795b54822eb800b79ac522d
marrel <marrel7pinto@gmail.com>
2024-08-15 20:20:56 +0100

    one  lab_id was added so that feedback of that lab is taken

---
+++ database/migrations/2024_08_15_102115_create_feedbacks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('labs_id')->unsigned();
+            $table->integer('f_understanding');
+            $table->string('f_confusing');
+            $table->text('f_interesting');
+            $table->text('f_engaging');
+            $table->string('f_important');
+            $table->integer('f_overall');
+            $table->integer('f_difficulty');
+            
+
+            $table->foreign('labs_id')->references('id')->on('labs')
+            ->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+};

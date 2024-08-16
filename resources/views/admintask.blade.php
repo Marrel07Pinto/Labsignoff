6e200bc958f79cf4eea8349bbeaad94b2d12b2f6
marrel <marrel7pinto@gmail.com>
2024-08-16 18:28:40 +0100

    deleted as it was not needed

--- resources/views/admintask.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.admin')
-@section('content')
-<body>
-    <main id="main" class="main">
-        <div class="pagetitle">
-            <h1>Please upload the task</h1>
-        </div>
-
-@endsection

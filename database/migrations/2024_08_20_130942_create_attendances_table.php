2a41cbf88dbdf4b983ab254b313b552673ae4cf1
marrel <marrel7pinto@gmail.com>
2024-08-20 15:55:14 +0100

    added attributes to attendence table

---
+++ database/migrations/2024_08_20_130942_create_attendances_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->string('u_num');
+            $table->string('lab');
+            $table->string('atten');
+
+            $table->foreign('u_num')->references('u_num')->on('users')
+            ->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};

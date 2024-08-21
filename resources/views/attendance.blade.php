83fc321d220d2a193509df4e5547e614b0235020
marrel <marrel7pinto@gmail.com>
2024-08-21 17:30:11 +0100

    created just a blade page

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -1,13 +1,6 @@
 @extends('layouts.admin')
 
 @section('content')
-<body>
-<main id="main" class="main">
-                <div class="pagetitle">
-                    <h1><center>Attendance</center></h1>
-                </div><!-- End Page Title -->
-                <section class="section">
-                </section>           
-        </main>
-</body>
+    
+<h1> Attendance</h1>
 @endsection

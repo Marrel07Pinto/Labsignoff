4196462d85a6cdf0190d33e169ecdf409f3b114c
marrel <marrel7pinto@gmail.com>
2024-08-21 09:42:11 +0100

    created attendance blade for admin side

---
+++ resources/views/attendance.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('content')
+<body>
+<main id="main" class="main">
+                <div class="pagetitle">
+                    <h1><center>Attendance</center></h1>
+                </div><!-- End Page Title -->
+                <section class="section">
+                </section>           
+        </main>
+</body>
+@endsection

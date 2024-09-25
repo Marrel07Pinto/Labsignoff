ec5650dd4310c571da6c9336b9f8d8c9dc40529b
marrel <marrel7pinto@gmail.com>
2024-09-25 10:15:37 +0100

    added content in Home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,11 @@
 @section('content')
 <!DOCTYPE html>
 <html>
+    <style>
+        p {
+            text-align: justify;
+           }
+    </style>
     <body>
         <main id="main" class="main">
 
@@ -10,8 +15,14 @@
             </div><!-- End Page Title -->
 
             <section class="section">
-            
-  
+            <p>A web application lab gives practical environment for students to develop and to test web applicatioin
+                    technologies like Javascript,HTML,CSS and web application framework. It helps student to think about real life scenarios,
+                    manage database, Build dynamic websites and security about web application. These labs gives practical knowege in 
+                    creating, deploying and to build a good web application interaction and cybersecuritry.<p>
+                <center><img src="/images/webapplication.jpg" alt="Web Application" width="300" height="300"></center>
+                <br>
+           <p>Web application is a program that runs on the internet, allowing user to interact with through web. Generally, software's are installed in the system but, 
+            web application are accessed online. some of the web application are Facebook, Gmail, YouTube. They are easy to us because they don't need to download.<p>
             </section>
             </main>
     </body>
